f9674d067da5afe188fbee9d6b97aeebebda82f5
mkabyken <kabykenov.miras@yandex.kz>
2023-10-06 18:32:41 +0600

    fixing

--- internal/handlers/signin.go
+++ internal/handlers/signin.go
@@ -1,23 +1,46 @@
 package handlers
 
 import (
-	"fmt"
+	"crypto/md5"
+	"encoding/hex"
 	"net/http"
+	"net/url"
+	"text/template"
+	"time"
 )
 
 func (h *Handler) signin(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	if r.URL.Path != "/signin" {
 		return
 	}
-
-	err := r.ParseForm()
+	ts, err := template.ParseFiles("./ui/templates/signin.html")
+	err = r.ParseForm()
 	if err != nil {
 		return
 	}
-	email := r.Form.Get("email")
-	password := r.Form.Get("password")
+	if r.Method == http.MethodPost {
+		email := r.Form.Get("email")
+		password := r.Form.Get("password")
 
-	id, err := h.Service.Authorization.GetUser(email, password)
-	cookie := http.Cookie{Name: "token"}
-	fmt.Println(id)
+		id, err := h.Service.Authorization.GetUser(email, password)
+		if err != nil {
+			return
+		}
+		if id > 0 {
+			time64 := time.Now().Unix()
+			timeInt := string(time64)
+			token := email + password + timeInt
+			hashToken := md5.Sum([]byte(token))
+			hashedToken := hex.EncodeToString(hashToken[:])
+			h.Cache[hashedToken] = id
+			livingTime := 60 * time.Minute
+			expiration := time.Now().Add(livingTime)
+			cookie := http.Cookie{Name: "Token", Value: url.QueryEscape(hashedToken), Expires: expiration}
+			http.SetCookie(w, &cookie)
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+		}
+
+	} else if r.Method == http.MethodGet {
+		ts.Execute(w, "")
+	}
 }
--- structs/user.go
+++ structs/user.go
@@ -1,7 +1,31 @@
 package structs
 
 type User struct {
-	Username       string
-	Email          string
-	HashedPassword string
+	username       string
+	email          string
+	hashedPassword string
+}
+
+func (u *User) GetUserName() string {
+	return u.username
+}
+
+func (u *User) GetUserEmail() string {
+	return u.email
+}
+
+func (u *User) GetUserHashPassword() string {
+	return u.hashedPassword
+}
+
+func (u *User) ChangeUserName(s string) {
+	u.username = s
+}
+
+func (u *User) ChangeUserEmail(s string) {
+	u.email = s
+}
+
+func (u *User) ChangeUserHashPassword(s string) {
+	u.hashedPassword = s
 }
